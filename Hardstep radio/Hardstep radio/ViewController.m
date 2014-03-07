ef29375464edda5ad5693320491a1187ca24fbc4
maloy10071991 <qwert1154@yandex.ru>
2014-03-07 17:53:26 +0400

    набросан интерфейс и связи

--- Hardstep radio/Hardstep radio/ViewController.m
+++ Hardstep radio/Hardstep radio/ViewController.m
@@ -13,17 +13,16 @@
 @end
 
 @implementation ViewController
+@synthesize allowsAirPlay, nowplaying, toolBar, playButton, pauseButton, movieDuration;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
