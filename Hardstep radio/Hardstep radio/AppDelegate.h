fdbdcd88ca345fd8eead2e430d0dcb48afb8e4b9
maloy10071991 <qwert1154@yandex.ru>
2014-03-07 13:46:14 +0400

    Создан проект

---
+++ Hardstep radio/Hardstep radio/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hardstep radio
+//
+//  Created by Родштейн Алексей on 07.03.14.
+//  Copyright (c) 2014 Alex Rodshtein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hardstep radio/Hardstep radio/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hardstep radio
+//
+//  Created by Родштейн Алексей on 07.03.14.
+//  Copyright (c) 2014 Alex Rodshtein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
