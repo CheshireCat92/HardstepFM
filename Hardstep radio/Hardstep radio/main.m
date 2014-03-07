38f06b5a385006fba22c7ece778190251a3c1e5c
maloy10071991 <qwert1154@yandex.ru>
2014-03-07 13:46:14 +0400

    Создан проект

---
+++ Hardstep radio/Hardstep radio/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hardstep radio
+//
+//  Created by Родштейн Алексей on 07.03.14.
+//  Copyright (c) 2014 Alex Rodshtein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Hardstep radio/Hardstep radioTests/Hardstep_radioTests.m
@@ -0,0 +1,34 @@
+//
+//  Hardstep_radioTests.m
+//  Hardstep radioTests
+//
+//  Created by Родштейн Алексей on 07.03.14.
+//  Copyright (c) 2014 Alex Rodshtein. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Hardstep_radioTests : XCTestCase
+
+@end
+
+@implementation Hardstep_radioTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
